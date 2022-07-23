5a73fdf474b87c10f551d6055c85cfb7d36cdf9e
shadowfaz16 <erikfazju@gmail.com>
2022-07-23 14:29:05 +0200

    2. Smoke Test

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+require("@nomicfoundation/hardhat-chai-matchers");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      localhost: {},
+    },
+  },
 };
